49f47a9d03c3dcedea9a68006a0255fd899be41f
Wilker Brito Dias <wilkerwbddias@gmail.com>
2025-07-05 09:07:46 -0300

    at15.php

--- Exercicios 3 php/15/at15.php
+++ Exercicios 3 php/15/at15.php
@@ -9,12 +9,17 @@
     - Acima de R$ 5.000 → 15%
  */
 $venda = $_GET["venda"];
+$com1 = $venda * (5 / 100);
+$com2 = $venda * (10 / 100);
+$com3 = $venda * (15 / 100);
+
+
 
 if ($venda <= 1000) { 
-    echo "sua comissão sera de 5%"; 
+    echo "sua comissão será de 5%, tendo um valor de $com1 "; 
 }elseif (($venda >= 1001) && ($venda<=5000)) {
-     echo "Sua comissão será de 10%";
+     echo "Sua comissão será de 10%, tendo um valor de $com2";
 }elseif ($venda > 5000) {
-     echo " sua comissão será de 15%";
+     echo " sua comissão será de 15%, tendo um valor de $com3";
 }
 ?>
